66e60a6126d4cbd96fcf81edd123778f35fa0138
LHernZ <luisfernando.imt@gmail.com>
2022-03-08 12:07:08 -0600

    Add indent_blankline

--- nvim/.config/nvim/lua/packer-config/init.lua
+++ nvim/.config/nvim/lua/packer-config/init.lua
@@ -67,6 +67,8 @@ return require("packer").startup(function(use)
 
   use ('Shatur/neovim-ayu')
 
+  use('lukas-reineke/indent-blankline.nvim')
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if packer_bootstrap then
