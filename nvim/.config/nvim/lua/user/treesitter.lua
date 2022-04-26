10571966b2ca16f20cfe7dc790de4ffe1e96fd0b
LHernZ <luisfernando.imt@gmail.com>
2022-04-26 16:54:01 -0500

    (feature): Add autoclose tags

--- nvim/.config/nvim/lua/user/treesitter.lua
+++ nvim/.config/nvim/lua/user/treesitter.lua
@@ -1,11 +1,13 @@
 local configs = require("nvim-treesitter.configs")
-configs.setup {
-  ensure_installed = "maintained",
-  sync_install = false, 
-  highlight = {
-    enable = true, -- false will disable the whole extension
-    additional_vim_regex_highlighting = true,
-
-  },
-  indent = { enable = true, disable = { "yaml" } },
-}
+configs.setup({
+	ensure_installed = "all",
+	sync_install = false,
+	highlight = {
+		enable = true, -- false will disable the whole extension
+		additional_vim_regex_highlighting = true,
+	},
+	indent = { enable = true, disable = { "yaml" } },
+	autotag = {
+		enable = true,
+	},
+})
